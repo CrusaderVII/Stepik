67d77d34d6a6a47463c73fb61e8ae01a80909a0c
Egor Chervonikov <crusaderworkvii@gmail.com>
2022-09-01 22:43:19 +0300

    Add start

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setVisible(true);
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
